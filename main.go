41a40a95f7513185436f1290b65e1df7ca0ac7db
kidoda <kdevb0x@gmail.com>
2018-09-17 14:31:32 -0700

    start v1.0.0 bottom-up with fresh project

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
-package main // import "github.com/kidoda/quikalarm"
+package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/fyne-io/fyne/desktop"
+	"github.com/spf13/pflag"
 )
 
 func main() {
+	var Usage = `Usage:	quikalarm [options...] [arguments]
+	Simple alarm clock with few options.
 
-	app := desktop.NewApp()
-	mainWindow, err := CreateMainWindow(app, "QuikAlarm")
-	if err != nil {
-		log.Fatalf("Fatal error: %s", err)
-	}
+	Mandatory arguments to long options are mandatory for short options too.
+
+	Options:
+	    -z, --snooze-time 	set snooze duration; The snooze prompt will appear once the alert buzzer starts;
+				it defaults to 'YES', so just pressing 'ENTER' will snooze for this amount of time.
+
+	    -s, --set		set the alarm wake-up time; argument in 12h or 24h format,
+				in the form [hour:minute AM/PM]. (e.g., 9:00AM, 22:00, or 08:30PM)
+
+	    -t, --timer		timer mode; in this mode the arguments given to -s, --set will be treated
+	    			as a duration instead of an absolute time.`
+	var (
+		clock = NewClock()
+		buzz  = loadBuzzer()
+	)
 }
